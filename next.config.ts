0ab7a4b51b4eb299512f3fbad1b505fdb9984201
suraj kumar <surajk236789@gmail.com>
2026-07-18 22:54:28 +0530

    feat: add redirect from jsondiff.space to allformatter.com in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   compress: true,
-  allowedDevOrigins: ['*', '192.168.1.4']
+  allowedDevOrigins: ['*', '192.168.1.4'],
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'jsondiff.space',
+          },
+        ],
+        destination: 'https://www.allformatter.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
